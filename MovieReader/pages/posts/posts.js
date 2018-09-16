db4a3c30e2682f7369367353f5ab8e96fe7743f7
wangzhi102001 <wangzhi102001@live.com>
2018-09-16 20:34:47 +0800

    20180916

--- MovieReader/pages/posts/posts.js
+++ MovieReader/pages/posts/posts.js
@@ -1,18 +1,37 @@
 // pages/posts/posts.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var posts_content = [
+      {
+        date: "Sep 18 2016",
+        title: "张三李四王五",
+        post_img: "/images/posts/cat.png",
+        author_img: "/images/posts/avatar/1.png",
+        content: "ahidahkldjla立即受到了开发老树开花菲利克斯很快乐烦死了烦水利科技发生了纠纷几时放假上来就发生了纠纷生理健康",
+        view_num : "112",
+        collect_num :"96",
+      },
+      {
+        date: "Sep 12 2016",
+        title: "wqeqweqeqeq五",
+        post_img: "/images/posts/crab.png",
+        author_img: "/images/posts/avatar/2.png",
+        content: "asahdasdhjajkdah撒了姐姐啊来了多久啊垃圾生了纠纷几时放假上来就发生了纠纷生理健康",
+        view_num :"112",
+        collect_num : "96",
+      }
+    ]
+    this.setData({posts_key:posts_content});
   },
 
   /**
